ab03215bcc5a9be8531d448ed29df5eccdbf805a
polatengin <enpolat@microsoft.com>
2021-01-23 23:41:02 +0300

    index of the project is renamed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { App } from './Views/App';
+
 import "./bundle.css"
 
 ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
