488934de31a8c1fb660ef20eed5d86841c3c259e
polatengin <enpolat@microsoft.com>
2021-01-23 23:14:12 +0300

    teams dropdown component created

---
+++ src/Components/TeamsDropdown.tsx
@@ -0,0 +1,78 @@
+import React, { FunctionComponent, useState } from 'react';
+
+export const TeamsDropdown: FunctionComponent = () => {
+
+  const [ isOpen, setIsOpen ] = useState(false);
+
+  return (
+    <>
+      <div className="relative w-80">
+        <button onClick={() => { setIsOpen(!isOpen); }} type="button" className="teams-dropdown">
+          <span className="flex items-center">
+            <svg className="flex-shrink-0 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+            <span className="ml-3 block truncate">
+              Tom Cook
+            </span>
+          </span>
+          <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+            <svg className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" />
+            </svg>
+          </span>
+        </button>
+        { isOpen &&
+        <div className="absolute w-full border border-gray-300 dark:border-gray-700 rounded-sm shadow-lg">
+          <div onClick={() => { setIsOpen(false); }} className="inset-0 fixed"></div>
+          <ul tabIndex={-1} role="listbox" className="max-h-56 rounded-sm text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+            <li className="hover:bg-gray-50 dark:hover:bg-gray-800 cursor-pointer select-none relative py-2 pl-3 pr-9">
+              <div className="flex items-center">
+                <svg className="flex-shrink-0 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span className="ml-3 block font-normal truncate">
+                  Wade Cooper
+                </span>
+              </div>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-4">
+                <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" />
+                </svg>
+              </span>
+            </li>
+            <li className="hover:bg-gray-50 dark:hover:bg-gray-800 cursor-pointer select-none relative py-2 pl-3 pr-9">
+              <div className="flex items-center">
+                <svg className="flex-shrink-0 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span className="ml-3 block font-normal truncate">
+                  Wade Cooper
+                </span>
+              </div>
+            </li>
+            <li className="hover:bg-gray-50 dark:hover:bg-gray-800 cursor-pointer select-none relative py-2 pl-3 pr-9">
+              <div className="flex items-center">
+                <svg className="flex-shrink-0 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span className="ml-3 block font-normal truncate">
+                  Wade Cooper
+                </span>
+              </div>
+            </li>
+            <li className="hover:bg-gray-50 dark:hover:bg-gray-800 cursor-pointer select-none relative py-2 pl-3 pr-9">
+              <div className="flex items-center">
+                <svg className="flex-shrink-0 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span className="ml-3 block font-normal truncate">
+                  Wade Cooper
+                </span>
+              </div>
+            </li>
+            <li className="hover:bg-gray-50 dark:hover:bg-gray-800 cursor-pointer select-none relative py-2 pl-3 pr-9">
+              <div className="flex items-center">
+                <svg className="flex-shrink-0 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span className="ml-3 block font-normal truncate">
+                  Wade Cooper
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        }
+      </div>
+    </>
+  );
+};
