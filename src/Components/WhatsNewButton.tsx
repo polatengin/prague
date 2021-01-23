58eda1bcc77ece2b5e8387ab59bbf19b8aaefd74
polatengin <enpolat@microsoft.com>
2021-01-23 23:16:53 +0300

    state added and modal dialog show on screen based on the value in the state

--- src/Components/WhatsNewButton.tsx
+++ src/Components/WhatsNewButton.tsx
@@ -4,8 +4,11 @@ import { Modal } from './Modal';
 
 export const WhatsNewButton: FunctionComponent = () => {
 
+  const [ isModalOpen, setIsModalOpen ] = useState(false);
+
   return (
     <>
+      { isModalOpen &&
         <Modal
           title="What's New"
           body="With version 1.0.50 Retrospective Session participants may revoke already casted votes. Max vote limit will come with version 1.0.51 🥳"
@@ -28,8 +31,11 @@ export const WhatsNewButton: FunctionComponent = () => {
           }
           onDismiss={() => { setIsModalOpen(false); }}
         />
+      }
+      <div onClick={() => { setIsModalOpen(!isModalOpen); }} className="flex items-center cursor-pointer">
         <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" /></svg>
         <span className="ml-1">What's New</span>
+      </div>
     </>
   );
 };
