2f5e24038559ffdd51c18f01903bd4a4919bfd20
polatengin <enpolat@microsoft.com>
2021-01-23 22:35:31 +0300

    main entry point of the react app

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './Views/App';
+import "./bundle.css"
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
