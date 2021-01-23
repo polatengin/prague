f394aef64b075c46aa29e5f228129ffa6b5198a3
polatengin <enpolat@microsoft.com>
2021-01-23 17:00:41 +0300

    colors defined to use in the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class',
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      blue: colors.blue,
+      yellow: colors.yellow,
+    },
     extend: {},
   },
   variants: {
