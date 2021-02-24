ffcd3e16fdf0b4d75dac04f9360a9359c3dd8359
polatengin <enpolat@microsoft.com>
2021-02-24 11:51:54 +0300

    styles for the team dropdown inlined

--- src/Components/TeamsDropdown.tsx
+++ src/Components/TeamsDropdown.tsx
@@ -7,7 +7,7 @@ export const TeamsDropdown: FunctionComponent = () => {
   return (
     <>
       <div className="relative w-80">
-        <button onClick={() => { setIsOpen(!isOpen); }} type="button" className="teams-dropdown">
+        <button onClick={() => { setIsOpen(!isOpen); }} type="button" className="relative hover:bg-gray-50 dark:hover:bg-gray-800 cursor-pointer w-full bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-sm shadow-sm pl-3 pr-10 py-1 text-left outline-none focus:outline-none sm:text-sm">
           <span className="flex items-center">
             <svg className="flex-shrink-0 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
             <span className="ml-3 block truncate">
