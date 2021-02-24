796868d0540fbb91e30ead356d50ac604d073556
polatengin <enpolat@microsoft.com>
2021-02-24 11:48:37 +0300

    styles fixed for add new feedback button

--- src/Components/Column.tsx
+++ src/Components/Column.tsx
@@ -17,7 +17,7 @@ export const Column: FunctionComponent<Props> = (props: Props) => {
         <svg className="w-7 h-7 p-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
         </svg>
-        <span className="text-white text-sm">Add new feedback</span>
+        <span className="dark:text-white text-gray-800 text-sm">Add new feedback</span>
       </div>
     </>
   );
