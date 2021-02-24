548929cf643bf7033080a9774bde1865f18f6ca8
polatengin <enpolat@microsoft.com>
2021-02-24 11:49:15 +0300

    column icon changed

--- src/Views/App.tsx
+++ src/Views/App.tsx
@@ -32,7 +32,7 @@ export const App: FunctionComponent = () => {
           <div className="bg-gray-100 dark:bg-gray-800">
             <Column
               icon={
-                <svg className="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" /></svg>
+                <svg className="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
               }
               title="Column 1"
             />
