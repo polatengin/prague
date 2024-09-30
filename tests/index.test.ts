d46d2d576e8c76142f8c842bdd0bd630eede0e7d
polatengin <enpolat@microsoft.com>
2024-09-30 10:47:22 -0700

    adding empty given account name state

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,3 +2,16 @@ import { describe, it } from 'node:test';
 import { strictEqual } from 'node:assert';
 
 import { parse } from '../src/inputs';
+
+describe('index.ts', () => {
+  it('should exit with error when no GitHub account name is provided', () => {
+    // Arrange
+    const expected = 'Please provide a GitHub account name.';
+    let actual = '';
+
+    // Act
+    actual = parse('');
+
+    // Assert
+    strictEqual(actual, expected);
+  });
