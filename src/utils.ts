3bf8099a2adbde1d0fb21b3c256ef0de7100acb6
polatengin <enpolat@microsoft.com>
2024-09-30 12:01:58 -0700

    adding github value details type

--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,25 @@ export const validateAccountName = (value: string) => {
 
   return '';
 }
+
+export type GitHubValueDetails = {
+  name: string;
+  public_repos_count: number;
+  public_gists_count: number;
+  followers_count: number;
+  created_at: Date;
+  orgs: {
+    public_repos_count: number;
+    public_gists_count: number;
+    followers_count: number;
+    created_at: Date;
+  }[];
+  repos: {
+    stargazers_count: number;
+    watchers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    subscribers_count: number;
+    created_at: Date;
+  }[];
+};
